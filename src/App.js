ee08b107a6781413e55248965131087b124ca2df
xovest <79008917+xovest@users.noreply.github.com>
2021-11-06 23:20:48 +0200

    added the searchbar but the button is lil invisible but it still works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -14,15 +15,17 @@ function App() {
         setIsLoading(false);
       })
       .catch(err => console.log(err));
-  }, []);
+  }, [term]);
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
+      <ImageSearch searchText={text => setTerm(text)} />
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No images found!:(</h1>}
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(img => (
           <ImageCard key={img.id} image={img} />
         ))}
-      </div>
+      </div>}
     </div>
   );
 }
